f98f648321edc341b83801e248fcbddba1713244
jakhongir777 <jakhogirmirzo.tursunaliev@mail.ru>
2021-02-04 18:10:45 +0500

    Dss

--- components/Sign/SignUp.vue
+++ components/Sign/SignUp.vue
@@ -37,7 +37,7 @@
         <!-- <Input type="email" inputPlaceholder="Email Address" /> -->
         <Input
           type="tel"
-          inputPlaceholder="Phone_nmber"
+          inputPlaceholder="Phone number"
           v-model="form.phone_number"
         />
         <Input
@@ -51,7 +51,7 @@
             Service & Privacy Policy.
           </p>
           <Button type="submit" @click="getCode" btnStyle="controlButtonSubmit"
-            >Sign up</Button
+            >Continue</Button
           >
           <check-code @codeTransfer="checkCode" />
         </div>
@@ -61,7 +61,9 @@
 </template>
 
 <script>
+import Button from '../ControlFields/Button.vue'
 export default {
+  components: { Button },
   props: {},
   data() {
     return {
@@ -90,6 +92,13 @@ export default {
         })
         .catch((err) => {
           console.log('[GET CODE ERROR]', err)
+          this.showToast(
+            'danger',
+            'b-toaster-bottom-right',
+            '2000',
+            'xatolik',
+            'Telefon raqam yoki parol xato terilgan'
+          )
         })
     },
     async checkCode(payload) {
@@ -101,7 +110,9 @@ export default {
         .then((res) => {
           this.form.token = res.data.token
         })
-        .catch((err) => console.log(err))
+        .catch((err) => {
+          console.log(err)
+        })
 
       if (this.form.token != '' && this.form.token != null) {
         await this.$axios
@@ -109,6 +120,9 @@ export default {
           .then((res) => {
             console.log('Final user/: ', res)
             this.$auth.loginWith('local', { data: this.form })
+            this.$nextTick(() => {
+              this.showToast('success', 'b-toaster-bottom-right', '2000')
+            })
           })
           .catch((err) => console.log('[USER ERROR]', err))
         this.form = {
@@ -118,11 +132,36 @@ export default {
           password: '',
           token: '',
         }
-        this.$router.push('/')
+        this.$router.push('/about')
+        // this.showToast('success', 'b-toaster-bottom-right', '2000')
       }
-
       this.$nextTick(() => {
         this.$bvModal.hide('modal-check-code')
+      })
+    },
+
+    showToast(variant, toaster, autoHideDelay, title, message) {
+      // Use a shorter name for this.$createElement
+      const h = this.$createElement
+      // Create the message
+      const vNodesMsg = h('p', { class: ['text-center', 'mb-0'] }, [
+        h('b-spinner', { props: { type: 'grow', small: true } }),
+        message,
+        h('b-spinner', { props: { type: 'grow', small: true } }),
+      ])
+      // Create the title
+      const vNodesTitle = h(
+        'div',
+        { class: ['d-flex', 'flex-grow-1', 'align-items-baseline', 'mr-2'] },
+        [h('strong', { class: 'mr-2' }, title)]
+      )
+      // Pass the VNodes as an array for message and title
+      this.$bvToast.toast([vNodesMsg], {
+        title: [vNodesTitle],
+        solid: true,
+        variant: variant,
+        toaster: toaster,
+        autoHideDelay: autoHideDelay,
       })
     },
   },
