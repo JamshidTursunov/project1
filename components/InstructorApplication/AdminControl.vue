f1c7480f8228e49031bfaeddc5721ca100398bc8
jakhongir777 <jakhogirmirzo.tursunaliev@mail.ru>
2021-02-15 12:22:32 +0500

    SsdKds

---
+++ components/InstructorApplication/AdminControl.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    AdminControl
+    <button @click="control">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    async control() {},
+  },
+}
+</script>
+
+<style></style>
--- components/InstructorApplication/InstructorApplication.vue
+++ components/InstructorApplication/InstructorApplication.vue
@@ -8,9 +8,18 @@
       </p>
     </div>
     <form>
-      <Input inputPlaceholder="First Name" />
-      <Input inputPlaceholder="Last Name" />
-      <Input type="email" inputPlaceholder="Email Address" />
+      <Input v-model="form.first_name" inputPlaceholder="First Name" />
+      <Input v-model="form.last_name" inputPlaceholder="Last Name" />
+      <Input
+        v-model="form.phone_number"
+        type="tel"
+        inputPlaceholder="Phone number"
+      />
+      <Input
+        v-model="form.password"
+        type="password"
+        inputPlaceholder="Password"
+      />
       <div class="inputControl__box">
         <input
           type="file"
@@ -23,8 +32,12 @@
           <span>{{ label }}</span> <span>Upload file</span>
         </label>
       </div>
-      <Input inputPlaceholder="Send link of your portfolio" />
       <Input
+        v-model="form.partifol"
+        inputPlaceholder="Send link of your portfolio"
+      />
+      <Input
+        v-model="form.description"
         controlType="textarea"
         textPlaceholder="Short information about your skills"
         rows="7"
@@ -41,16 +54,29 @@
 </template>
 
 <script>
+import Toast from '~/utils/toast.js'
 export default {
+  mixins: [Toast],
   props: {},
   data() {
     return {
       label: 'Send your resume',
+      form: {
+        first_name: 'Jahongir',
+        last_name: 'Tursunaliev',
+        phone_number: '+998991000544',
+        password: '23012001',
+        resume: 'dog.mp4',
+        partifol: 'www.smth.com',
+        description: 'I am a developer',
+        token: '',
+      },
     }
   },
   methods: {
     valChange(event) {
       this.label = event.target.files[0].name
+      this.form.resume = event.target.files[0].name
     },
   },
 }
