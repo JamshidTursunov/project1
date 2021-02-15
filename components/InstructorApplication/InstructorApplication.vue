9a28017236cc183795aae1ce65dc9d55b07b7526
jakhongir777 <jakhogirmirzo.tursunaliev@mail.ru>
2021-02-15 14:37:59 +0500

    sdsd

--- components/InstructorApplication/InstructorApplication.vue
+++ components/InstructorApplication/InstructorApplication.vue
@@ -7,7 +7,7 @@
         your career.
       </p>
     </div>
-    <form>
+    <form @submit.prevent="getCode" enctype="multipart/form-data">
       <Input v-model="form.first_name" inputPlaceholder="First Name" />
       <Input v-model="form.last_name" inputPlaceholder="Last Name" />
       <Input
@@ -49,6 +49,7 @@
         </p>
         <Button type="submit" btnStyle="controlButtonSubmit">Submit</Button>
       </div>
+      <check-code @codeTransfer="checkCode" />
     </form>
   </div>
 </template>
@@ -57,26 +58,92 @@
 import Toast from '~/utils/toast.js'
 export default {
   mixins: [Toast],
-  props: {},
   data() {
     return {
       label: 'Send your resume',
       form: {
         first_name: 'Jahongir',
         last_name: 'Tursunaliev',
-        phone_number: '+998991000544',
+        phone_number: '+998903444447',
         password: '23012001',
-        resume: 'dog.mp4',
         partifol: 'www.smth.com',
         description: 'I am a developer',
         token: '',
       },
+      resume: '',
     }
   },
   methods: {
     valChange(event) {
       this.label = event.target.files[0].name
-      this.form.resume = event.target.files[0].name
+      console.log(event.target.files[0])
+      this.form.resume = event.target.files[0]
+      console.log(this.form)
+    },
+
+    async getCode() {
+      await this.$axios
+        .post('user/send/code/', {
+          phone_number: this.form.phone_number,
+        })
+        .then((res) => {
+          console.log('[Sent code]', res.data.code)
+          this.$nextTick(() => {
+            this.$bvModal.show('modal-check-code')
+          })
+        })
+        .catch((err) => {
+          console.log('[GET CODE ERROR]', err)
+          this.showToast('danger', 'Xatolik', "Anketa to'gri to'ldirilmagan")
+        })
+    },
+    async checkCode(payload) {
+      await this.$axios
+        .post('user/check/code/', {
+          phone_number: this.form.phone_number,
+          code: payload,
+        })
+        .then((res) => {
+          console.log(res.data)
+          this.form.token = res.data.token
+          console.log(this.form.token)
+          console.log(this.form)
+        })
+        .catch((err) => {
+          console.log(err)
+          this.showToast('danger', 'Xatolik', 'Kod xato terilgan')
+        })
+
+      if (this.form.token != '' && this.form.token != null) {
+        const resumeFile = new FormData()
+        resumeFile.append('form', this.resume)
+        console.log(resumeFile)
+        await this.$axios
+          .post('mentor/create/', {
+            headers: { 'Content-Type': 'multipart/form-data' },
+          })
+          .then((res) => {
+            console.log(res.data)
+            // console.log('Final user/: ', res.data)
+            // this.$auth.loginWith('local', { data: this.form })
+            // this.$router.push('/')
+            // this.$store.dispatch('course/initToastShow', true)
+          })
+          .catch((err) => console.log('[USER ERROR]', err))
+        this.form = {
+          first_name: '',
+          last_name: '',
+          phone_number: '',
+          password: '',
+          resume: '',
+          partifol: '',
+          description: '',
+          token: '',
+        }
+      }
+      this.$nextTick(() => {
+        this.$bvModal.hide('modal-check-code')
+      })
     },
   },
 }
